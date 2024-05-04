b78221ee8c22b0a8daf1078b78c5eca9f26d745c
Silviu Bouleanu <silviu_conte2006@yahoo.com>
2024-05-04 15:51:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const dino = document.querySelector('.dino');
+const grid = document.querySelector('.grid');
+const alert = document.getElementById('alert');
+let isJumping = false;
+let gravity = 0.9;
+let position = 0;
+let isGameOver = false;
+let score = 0;
+let hiScore = 0;
+
+function control(e) {
+    if (e.keyCode === 32) {
+        console.log("jump");
+        if (!isJumping && !isGameOver) {
+            isJumping = true;
+            jump();
+        }
+    }
+}
+
+document.addEventListener('keypress', control);
+
+function jump() {
+    let count = 0;
+    let timerID = setInterval(function () {
+        if (count === 15) {
+            clearInterval(timerID);
+            let downTimerID = setInterval(function () {
+                if (count === 0) {
+                    clearInterval(downTimerID);
+                    isJumping = false;
+                }
+                position -= 4.8;
+                count--;
+                position = position * gravity;
+                if (position < 0) position = 0;
+                dino.style.bottom = position + 'px';
+            }, 20);
+        }
+        count++;
+        position += 30;
+        position = position * gravity;
+        dino.style.bottom = position + 'px';
+    }, 20);
+}
+
+function generateObstacle() {
+    let randomTime = Math.random() * 4000;
+    let obstaclePosition = 1000;
+    const obstacle = document.createElement('div');
+    if (!isGameOver) obstacle.classList.add('obstacle');
+    grid.appendChild(obstacle);
+    obstacle.style.left = obstaclePosition + 'px';
+    let timerID = setInterval(function () {
+        if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+            clearInterval(timerID);
+            alert.innerHTML = 'GAME OVER';
+            isGameOver = true;
+        }
+        obstaclePosition -= 10;
+        obstacle.style.left = obstaclePosition + 'px';
+        if (obstaclePosition == 0) {
+            if (!isGameOver) {
+                score++;
+                if (score > hiScore) hiScore = score;
+                alert.innerHTML = score;
+            }
+        }
+    }, 20);
+
+    if (!isGameOver) setTimeout(generateObstacle, randomTime);
+}
+
+generateObstacle();
